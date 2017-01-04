13072c558010cc780392ba7bc0c960b69757e15f
silllvio <silllvio@hotmail.com>
2017-01-04 21:06:42 -0200

    autalização

---
+++ src/main/java/br/com/teste/conf/SpringSecurityFilterConfiguration.java
@@ -0,0 +1,8 @@
+package br.com.teste.conf;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer
+{
+
+}
